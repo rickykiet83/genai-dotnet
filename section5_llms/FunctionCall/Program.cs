f8ad3a23c0f6251a1fd6d181001e6afc987fad65
Kiet Pham <kietpham.dev@gmail.com>
2026-07-29 17:26:57 +1000

    Generating Embeddings

---
+++ section5_llms/FunctionCall/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+// get credentials from user secrets
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ??
+                                      throw new InvalidOperationException(
+                                          "Missing configuration: GitHubModels:Token."));
+var endpoint = config["GitHubModels:Endpoint"] ??
+               throw new InvalidOperationException("Missing configuration: GitHubModels:Endpoint.");
+var model = config["GitHubModels:Model"] ??
+            throw new InvalidOperationException("Missing configuration: GitHubModels:Model.");
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri(endpoint)
+};
+
+// create a chat client
+IChatClient client = new ChatClientBuilder(new OpenAIClient(credential, options)
+        .GetChatClient(model).AsIChatClient())
+    .UseFunctionInvocation()
+    .Build();
+
+var chatOptions = new ChatOptions
+{
+    Tools =
+    [
+        AIFunctionFactory.Create((string location, string unit) =>
+            {
+                // Here you would call a weather API to get the weather for the location
+                var temperature = Random.Shared.Next(5, 20);
+                var conditions = Random.Shared.Next(0, 1) == 0 ? "sunny" : "rainy";
+
+                return $"The weather is {temperature} degress C and {conditions} in {location}.";
+            },
+            "get_current_weather",
+            "Get the current weather in a given location.")
+    ]
+};
+
+
+List<ChatMessage> chatHistory =
+[
+    new(ChatRole.System, """
+                         You are a hiking enthusiast who helps people discover fun hikes in their area. 
+                         You are upbeat and friendly.
+                         """)
+];
+
+// Weather conversation relevant to the registered function.
+chatHistory.Add(new(ChatRole.User, """
+                                   I live in Wollongong Australia and I'm looking for a moderate intensity hike. 
+                                   What's the current weather like? 
+                                   """));
+
+Console.WriteLine($"{chatHistory.Last().Role} >>> {chatHistory.Last()}");
+
+ChatResponse response = await client.GetResponseAsync(chatHistory, chatOptions);
+
+chatHistory.Add(new(ChatRole.Assistant, response.Text));
+
+Console.WriteLine($"{chatHistory.Last().Role} >>> {chatHistory.Last()}");
