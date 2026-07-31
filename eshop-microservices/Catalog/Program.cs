1347cbecb90955010da6b9c518d2ba96aa6f491b
Kiet Pham <kietpham.dev@gmail.com>
2026-07-31 14:48:19 +1000

    add chat client to catalog service

--- eshop-microservices/Catalog/Program.cs
+++ eshop-microservices/Catalog/Program.cs
@@ -1,4 +1,8 @@
 
+using System.ClientModel;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,27 @@ builder.AddServiceDefaults();
 builder.AddNpgsqlDbContext<CatalogDbContext>(connectionName: "catalogdb");
 
 builder.Services.AddScoped<ProductService>();
+
+// Add AI Chat Client
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ??
+                                      throw new InvalidOperationException(
+                                          "Missing configuration: GitHubModels:Token."));
+var endpoint = config["GitHubModels:Endpoint"] ??
+               throw new InvalidOperationException("Missing configuration: GitHubModels:Endpoint.");
+var model = config["GitHubModels:Model"] ??
+            throw new InvalidOperationException("Missing configuration: GitHubModels:Model.");
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri(endpoint)
+};
+
+// create a chat client
+IChatClient chatClient =
+    new OpenAIClient(credential, options).GetChatClient(model).AsIChatClient();
+
+builder.Services.AddChatClient(chatClient);
 
 var app = builder.Build();
 
