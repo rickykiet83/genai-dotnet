597efa231683d074d607c082ec7e935c28c7f9e4
Kiet Pham <kietpham.dev@gmail.com>
2026-08-05 23:08:57 +1000

    Blazor FrontEnd Search Page Development

--- eshop-microservices/WebApp/ApiClients/CatalogApiClient.cs
+++ eshop-microservices/WebApp/ApiClients/CatalogApiClient.cs
@@ -14,9 +14,14 @@ public class CatalogApiClient(HttpClient httpClient)
         return response!;
     }    
 
-    public async Task<List<Product>?> SearchProducts(string query)
+    public async Task<List<Product>?> SearchProducts(string query, bool aiSearch)
     {
-        return await httpClient.GetFromJsonAsync<List<Product>>($"/products/search/{query}");       
+        if (aiSearch)
+        {
+            return await httpClient.GetFromJsonAsync<List<Product>>($"/products/aisearch/{query}");
+        }
+
+        return await httpClient.GetFromJsonAsync<List<Product>>($"/products/search/{query}");
     }
     
     public async Task<string?> SupportAgent(string query)
